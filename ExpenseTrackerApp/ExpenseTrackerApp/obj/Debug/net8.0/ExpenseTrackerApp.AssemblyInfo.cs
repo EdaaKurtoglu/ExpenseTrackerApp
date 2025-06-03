affee3e011f512f8a4e62720c0273f70c41725fd
EdaaKurtoglu <120361946+EdaaKurtoglu@users.noreply.github.com>
2025-06-03 23:40:22 +0300

    All pages are done.

--- ExpenseTrackerApp/ExpenseTrackerApp/obj/Debug/net8.0/ExpenseTrackerApp.AssemblyInfo.cs
+++ ExpenseTrackerApp/ExpenseTrackerApp/obj/Debug/net8.0/ExpenseTrackerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrackerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cc1d0b42e0a31ec6161fa025d4f44b4ac39cadd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+220f826046333870bb1df0e2b629653c7afc6bfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrackerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTrackerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
